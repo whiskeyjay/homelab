eafcb41e369871c0f1952e2bdb3a5dd213c74773
Jay Wang <jiewang@outlook.com>
2025-04-17 02:01:26 -0700

    Update Linux launch script

--- portainer/run.sh
+++ portainer/run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
 # Set environment variables for the application
 
-export PORTAINER_URL=http://your-portainer-url
-export PORTAINER_TOKEN_FILE=your-portainer-token
-export INFLUXDB_URL=http://your-influxdb-url
-export INFLUXDB_TOKEN_FILE=your-influxdb-token
-export INFLUXDB_ORG=your-org
-export INFLUXDB_BUCKET=your-bucket
+export PORTAINER_URL=https://portainer.wang-home.net
+export PORTAINER_TOKEN_FILE=$SCRIPT_DIR/.secrets/portainer
+export INFLUXDB_URL=https://influxdb.wang-home.net
+export INFLUXDB_TOKEN_FILE=$SCRIPT_DIR/.secrets/influxdb
+export INFLUXDB_ORG=18315178b6b5bc7e
+export INFLUXDB_BUCKET=a4e04404f8dd4cdb
 export POLL_INTERVAL_SECONDS=10
 
 echo "Environment variables set."
