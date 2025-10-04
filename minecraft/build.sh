f279fec7f9dea1067efac6b9155c766e1644f134
Jay Wang 🦀 <jiewang@outlook.com>
2025-10-04 01:10:49 -0700

    Minecraft Java Server 1.21.9

--- minecraft/build.sh
+++ minecraft/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker login -u whiskeyjay
-docker buildx build -t whiskeyjay/whmcsvr:latest -t whiskeyjay/whmcsvr:v1.21.8  --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t whiskeyjay/whmcsvr:latest -t whiskeyjay/whmcsvr:v1.21.9  --platform linux/amd64,linux/arm64 . --push
