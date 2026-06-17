1412b2d9374b03f343716524a0e34aed1c68eec5
Jay Wang <jiewang@outlook.com>
2026-06-16 21:36:28 -0700

    Minecraft Java Server 26.2

--- minecraft/build.sh
+++ minecraft/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker login -u whiskeyjay
-docker buildx build -t whiskeyjay/whmcsvr:latest -t whiskeyjay/whmcsvr:v26.1.2 --platform linux/amd64,linux/arm64 . --push
+docker buildx build -t whiskeyjay/whmcsvr:latest -t whiskeyjay/whmcsvr:v26.2.0 --platform linux/amd64,linux/arm64 . --push
